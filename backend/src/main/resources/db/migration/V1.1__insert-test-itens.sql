15e615049801931a0bf2e1ea08396156779103a7
Hernandes S. Pereira <hpereira23@outlook.com>
2026-03-02 15:36:19 -0300

    add db data test migration

---
+++ backend/src/main/resources/db/migration/V1.1__insert-test-itens.sql
@@ -0,0 +1,10 @@
+--DADOS DE TESTE
+INSERT INTO project (nome,descricao) VALUES
+    ('ERP','Projeto empresarial'),
+    ('Infra+','Projeto de redes');
+
+INSERT INTO task (titulo,descricao,concluido,project_id) VALUES
+    ('Modulo Comercial','Sprint de desenvolvimento do modulo comercial do ERP',false,1),
+    ('Landing Page','Sprint de desenvolvimento da landing page',true,1),
+    ('Crimpagem dos cabos','Preparação e armazenamento dos cabos RJ45',false,1),
+    ('Instalação - Switch','Instalação do Switch',true,2);
