36d7f2dfa3577d79e10f6b08d08a14ccbfbcfe09
Hernandes S. Pereira <hpereira23@outlook.com>
2026-03-02 15:43:17 -0300

    Revert "fix db data test migration"
    This reverts commit df5312646254d6500d1ddfe7f85f90cf4b4a882a.

--- backend/src/main/resources/db/migration/V1.1__insert-test-itens.sql
+++ backend/src/main/resources/db/migration/V1.1__insert-test-itens.sql
@@ -6,5 +6,5 @@ INSERT INTO project (nome,descricao) VALUES
 INSERT INTO task (titulo,descricao,concluido,project_id) VALUES
     ('Modulo Comercial','Sprint de desenvolvimento do modulo comercial do ERP',false,1),
     ('Landing Page','Sprint de desenvolvimento da landing page',true,1),
-    ('Crimpagem dos cabos','Preparação e armazenamento dos cabos RJ45',false,2),
+    ('Crimpagem dos cabos','Preparação e armazenamento dos cabos RJ45',false,1),
     ('Instalação - Switch','Instalação do Switch',true,2);
