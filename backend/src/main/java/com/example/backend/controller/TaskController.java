8e938bf61c321b5aa2e1298063553ef3d516ceb9
Hernandes S. Pereira <hpereira23@outlook.com>
2026-03-02 15:34:20 -0300

    add TaskRepository class

--- backend/src/main/java/com/example/backend/controller/TaskController.java
+++ backend/src/main/java/com/example/backend/controller/TaskController.java
@@ -1,17 +1,25 @@
 package com.example.backend.controller;
 
 import com.example.backend.task.Task;
+import com.example.backend.task.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("tasks")
 public class TaskController {
 
-    @GetMapping
-    public void getAll(){
+    @Autowired
+    private TaskRepository repository;
 
-        Task task;
+    @GetMapping
+    public List<Task> getAll(){
+
+        List<Task> taskList = repository.findAll();
+        return taskList;
     }
 }
