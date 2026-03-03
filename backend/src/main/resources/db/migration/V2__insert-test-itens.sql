f6cb22e3a2e2e1634849a2daf771029425062a6d
Hernandes S. Pereira <hpereira23@outlook.com>
2026-03-03 00:17:48 -0300

    fix bd

--- backend/src/main/resources/db/migration/V2__insert-test-itens.sql
+++ backend/src/main/resources/db/migration/V2__insert-test-itens.sql
@@ -1,3 +1,7 @@
+INSERT INTO project (nome, descricao) VALUES
+    ('Projeto ERP', 'Desenvolvimento do sistema ERP'),
+    ('Infra+', 'Infraestrutura de rede');
+
 INSERT INTO task (titulo,descricao,concluido,project_id) VALUES
     ('Modulo Comercial','Sprint de desenvolvimento do modulo comercial do ERP',false,1),
     ('Landing Page','Sprint de desenvolvimento da landing page',true,1),
