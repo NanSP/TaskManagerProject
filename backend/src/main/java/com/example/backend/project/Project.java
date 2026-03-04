95420701fb11b7f2ff1e16b03a59073725f5155a
Hernandes S. Pereira <hpereira23@outlook.com>
2026-03-04 18:01:13 -0300

    add Project package base

---
+++ backend/src/main/java/com/example/backend/project/Project.java
@@ -0,0 +1,11 @@
+package com.example.backend.project;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table(name = "project")
+@Entity(name = "project")
+public class Project {
+
+
+}
