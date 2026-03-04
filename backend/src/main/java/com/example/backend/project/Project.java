8559c92da45657384b38dc3519c8df7cb0a86c98
Hernandes S. Pereira <hpereira23@outlook.com>
2026-03-04 18:14:38 -0300

    add Project class

--- backend/src/main/java/com/example/backend/project/Project.java
+++ backend/src/main/java/com/example/backend/project/Project.java
@@ -1,11 +1,29 @@
 package com.example.backend.project;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Table(name = "project")
 @Entity(name = "project")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Project {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String descricao;
 
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
