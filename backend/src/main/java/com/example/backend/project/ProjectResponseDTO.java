f4d8d3c925eddebed0bfbd9b8ee979713a4c6bcd
Hernandes S. Pereira <hpereira23@outlook.com>
2026-03-04 18:24:28 -0300

    add ProjectResponseDTO

--- backend/src/main/java/com/example/backend/project/ProjectResponseDTO.java
+++ backend/src/main/java/com/example/backend/project/ProjectResponseDTO.java
@@ -1,4 +1,10 @@
 package com.example.backend.project;
 
-public record ProjectResponseDTO() {
+import java.time.LocalDateTime;
+
+public record ProjectResponseDTO(Integer id, String nome, String descricao, LocalDateTime created_at) {
+
+    public ProjectResponseDTO(Project project){
+        this(project.getId(), project.getNome(), project.getDescricao(), project.getCreated_at());
+    }
 }
