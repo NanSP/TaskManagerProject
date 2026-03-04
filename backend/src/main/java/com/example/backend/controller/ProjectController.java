66760de653f7322eb71d2dd208016cb2a8e59d0c
Hernandes S. Pereira <hpereira23@outlook.com>
2026-03-04 18:40:36 -0300

    add GET by ID project

--- backend/src/main/java/com/example/backend/controller/ProjectController.java
+++ backend/src/main/java/com/example/backend/controller/ProjectController.java
@@ -1,11 +1,15 @@
 package com.example.backend.controller;
 
+import com.example.backend.project.Project;
 import com.example.backend.project.ProjectRepository;
 import com.example.backend.project.ProjectResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("projects")
@@ -22,8 +26,14 @@ public class ProjectController {
     }
 
     @GetMapping("/{id}")
-    public void getById(){
+    public ResponseEntity<?> getById(@PathVariable(value = "id") Integer id){
 
+        Optional<Project> project = repository.findById(id);
+        if(project.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Projeto não encontrado");
+        }
+        ProjectResponseDTO projectDTO = new ProjectResponseDTO(project.get());
+        return  ResponseEntity.ok(projectDTO);
     }
 
     @PostMapping
