9e7aca865e63d10cb16b92be44d6eda1c8e6450d
Hernandes S. Pereira <hpereira23@outlook.com>
2026-03-08 15:36:00 -0300

    update card styles

--- frontend/src/components/card/card.tsx
+++ frontend/src/components/card/card.tsx
@@ -1,17 +1,20 @@
-import './card.css'
+import "./card.css";
 
 interface CardProps {
-    titulo: string,
-    descricao: string,
-    concluido: boolean
+  titulo: string;
+  descricao: string;
+  concluido: boolean;
 }
 
-export function Card({titulo, descricao, concluido}: CardProps) {
+export function Card({ titulo, descricao, concluido }: CardProps) {
   return (
     <div className="card">
       <h2>{titulo}</h2>
-      <p><b>Descrição da task:</b>{descricao}</p>
-        <p>{concluido ? "Concluído" : "Não concluído"}</p>
+      <p>
+        <b>Descrição da task: </b>
+      </p>
+      <p className="description">{descricao}</p>
+      <p>{concluido ? "Concluído" : "Não concluído"}</p>
     </div>
-  )
+  );
 }
