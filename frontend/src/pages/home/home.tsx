85582257b3261e367c1805a28eed03580a8707fb
Hernandes S. Pereira <hpereira23@outlook.com>
2026-03-08 16:01:32 -0300

    add home page with tasks button

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,10 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import Header from "../../components/header/header";
 import "./home.css";
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <>
       <Header></Header>
+      <div className="conteiner">
+        <h1 className="home-title">Welcome to the Home Page</h1>
+        <p className="home-description">
+          This is the home page of our application. Here you can find various
+          features and information about our services. Feel free to explore and
+          enjoy your stay!
+        </p>
+        <button className="task-button" onClick={() => navigate("/tasks")}>
+          Tasks
+        </button>
+      </div>
     </>
   );
 }
